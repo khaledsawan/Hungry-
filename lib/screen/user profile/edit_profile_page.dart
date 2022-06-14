d388c7ef450f1ea420e79316babe267aef3e8e66
Hornet7 <khaled963sawan@gmail.com>
2022-06-14 13:46:39 -0700

    edit profile page

---
+++ lib/screen/user profile/edit_profile_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+       child: Text('edit profile page'),
+      ),
+    );
+  }
+}
