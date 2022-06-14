2189d5420ba1e31e33eaece1964525fdbcdd9e5f
Hornet7 <khaled963sawan@gmail.com>
2022-06-14 13:46:12 -0700

    about us page

---
+++ lib/screen/we/about_us_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class AboutUsPage extends StatelessWidget {
+  const AboutUsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(child: Text('about us'),));
+  }
+}
