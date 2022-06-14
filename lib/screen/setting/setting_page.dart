764c9e8bcc899c3fd492e88a4969979808a33f81
Hornet7 <khaled963sawan@gmail.com>
2022-06-14 11:37:40 -0700

    Setting page

--- lib/screen/setting/setting_page.dart
+++ lib/screen/setting/setting_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shop_delivery_system/utils/colors.dart';
 
+import '../../widgets/icon_than_text/icon_than_text.dart';
+
 class SettingPage extends StatelessWidget {
   SettingPage({Key? key}) : super(key: key);
 
@@ -24,7 +26,7 @@ class SettingPage extends StatelessWidget {
 // set up the AlertDialog
     AlertDialog alert = AlertDialog(
       title: const Text('Choose Your Language'),
-      content: Container(
+      content: SizedBox(
           width: double.maxFinite,
           child: ListView.separated(
               shrinkWrap: true,
@@ -34,14 +36,13 @@ class SettingPage extends StatelessWidget {
                   child: GestureDetector(
                     child: Text(locale[index]['name']),
                     onTap: () {
-// print(locale[index]['name']);
                       updateLanguage(locale[index]['locale']);
                     },
                   ),
                 );
               },
               separatorBuilder: (context, index) {
-                return Divider(
+                return const Divider(
                   color: Colors.blue,
                 );
               },
@@ -59,8 +60,10 @@ class SettingPage extends StatelessWidget {
 //ChangeLanguageAlertDialog End
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.white,
+        backgroundColor: Colors.white,
         appBar: AppBar(
           title: Center(
               child: Text(
@@ -70,27 +73,70 @@ class SettingPage extends StatelessWidget {
           backgroundColor: AppColors.mainColor,
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              'hello'.tr,
-              style: TextStyle(fontSize: 15),
+            SizedBox(
+              height: height * 0.05,
             ),
             SizedBox(
-              height: 10,
-            ),
-            Text(
-              'message'.tr,
-              style: TextStyle(fontSize: 20),
-            ),
+                width: width - 17,
+                height: height * 0.22,
+                child: Image.asset('assets/image/settings.png')),
+
             SizedBox(
-              height: 10,
+              height: height * 0.05,
             ),
-            ElevatedButton(
-                onPressed: () {
-                  ChangeLanguageAlertDialog(context);
-                },
-                child: Text('changelang'.tr)),
+           Container(
+             margin:const EdgeInsets.only(left: 40,right: 20),
+             child: SingleChildScrollView(
+               child: Column(
+                 children: [
+                   Icon_Than_Text(
+                        color: AppColors.mainColor,
+                        icon: Icons.language_outlined,
+                        text: 'language',
+                      ),
+                   SizedBox(
+                     height: height * 0.05,
+                   ),
+                   Icon_Than_Text(
+                     color: AppColors.mainColor,
+                     icon: Icons.person,
+                     text: 'Edit profile',
+                   ),
+                   SizedBox(
+                     height: height * 0.05,
+                   ),
+                   Icon_Than_Text(
+                     color: AppColors.mainColor,
+                     icon: Icons.dark_mode_outlined,
+                     text: 'change them ',
+                   ),
+                   SizedBox(
+                     height: height * 0.05,
+                   ),
+                   Icon_Than_Text(
+                     color: AppColors.mainColor,
+                     icon: Icons.support_outlined,
+                     text: 'Support',
+                   ),
+                   SizedBox(
+                     height: height * 0.05,
+                   ),
+                   Icon_Than_Text(
+                     color: AppColors.mainColor,
+                     icon: Icons.group_outlined,
+                     text: 'about Us',
+                   ),
+                   SizedBox(
+                     height: height * 0.05,
+                   ),
+
+                 ],
+               ),
+             ),
+           ),
+
           ],
         ));
   }
